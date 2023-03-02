b0453c13bcec150108c88b59f08cd3bebd455c1c
FMghaccount <farzinmossaed@gmail.com>
2023-03-02 18:24:18 +0330

    complete shopping-list and shopping-edit components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('سیب', 5),
+    new Ingredient('گوجه فرنگی', 10),
+  ];
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
