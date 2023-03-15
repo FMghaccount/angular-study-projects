22c13e389a15d3c62557017e8cb55e0615b53795
FMghaccount <farzinmossaed@gmail.com>
2023-03-15 14:15:46 +0330

    add TD-Form to shopping-edit component with validation for adding ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from '../../shared/models/ingredient.model';
 import { ShoppingListService } from './../../shared/services/shopping-list.service';
-import { Component, ViewChild, ElementRef } from '@angular/core';
+// import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,18 +10,31 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-  @ViewChild('amountInput') amountInput: ElementRef;
-  ingredientName: string;
-  ingredientAmount: number;
+  // @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('form') formData: NgForm;
+  // ingredientName: string;
+  // ingredientAmount: number;
   ingredient: Ingredient;
+  // submitted: boolean = false;
   constructor(private shoppingListService: ShoppingListService) { }
 
-  onAddIngredients() {
-    if (this.ingredientName && this.amountInput.nativeElement.value && this.amountInput.nativeElement.value > 0) {
-      this.ingredient = new Ingredient(this.ingredientName, this.amountInput.nativeElement.value)
+  // onAddIngredients() {
+  //   if (this.ingredientName && this.amountInput.nativeElement.value && this.amountInput.nativeElement.value > 0) {
+  //     this.ingredient = new Ingredient(this.ingredientName, +this.amountInput.nativeElement.value)
+  //     this.shoppingListService.addIngredients(this.ingredient)
+  //   }
+  //   else return
+  // }
+
+  onSubmit() {
+    if (this.formData.value.name && this.formData.value.amount > 0) {
+      console.log(this.formData.value.name + ' ' + this.formData.value.amount);
+
+      this.ingredient = new Ingredient(this.formData.value.name, +this.formData.value.amount)
       this.shoppingListService.addIngredients(this.ingredient)
     }
-    else return
+    // this.submitted = true;
+    this.formData.reset();
   }
 
 }
